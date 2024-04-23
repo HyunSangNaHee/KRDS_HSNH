780cb02f322e9133c3232668379a038d8c5dac55
wynsumhi <gusdk7244@gamil.com>
2024-04-23 15:28:27 +0900

    [STYLE] 코드복사시 애니메이션 해당 li 기능

--- js/copyCode.js
+++ js/copyCode.js
@@ -25,12 +25,21 @@ function copyModalContents(event) {
     document.body.removeChild(tempTextarea);
 
     console.log('복사되었습니다.');
-
-    
 }
 
 // 각 li 요소에 클릭 이벤트 리스너 추가
 var liElements = document.querySelectorAll('.search-list > .li');
 liElements.forEach(function (li) {
-    li.addEventListener('click', copyModalContents);
+    li.addEventListener('click', function() {
+        // 모든 li 요소에서 animate 클래스 제거
+        liElements.forEach(function (li) {
+            li.classList.remove('animate__animated', 'animate__bounce');
+        });
+
+        // 클릭된 요소에만 animate 클래스 추가
+        li.classList.add('animate__animated', 'animate__bounce');
+        
+        // 원하는 작업 수행
+        copyModalContents();
+    });
 });
